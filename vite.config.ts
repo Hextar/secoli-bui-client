9de8e25eb3918d432b3febc32655701b6652003e
hextar <st.salidu@gmail.com>
2022-05-03 11:41:26 +0200

    chore: increased png comrpession

--- vite.config.ts
+++ vite.config.ts
@@ -46,7 +46,7 @@ export default defineConfig({
       gifsicle: { optimizationLevel: 7, interlaced: false },
       optipng: { optimizationLevel: 7 },
       mozjpeg: { quality: 20 },
-      pngquant: { quality: [0.8, 0.9], speed: 4 },
+      pngquant: { quality: [0.8, 0.9], speed: 3, strip: true },
       svgo: {
         plugins: [{ name: 'removeViewBox' },
           { name: 'removeEmptyAttrs', active: false },
