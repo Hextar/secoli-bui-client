9e7683da9e90dd56d91b1ab4d7916a6ec0ff7e86
Stefano Salidu <st.salidu@gmail.com>
2022-09-17 22:53:37 +0200

    fix: solved infinite reloading when incoming from FB

--- src/composables/useForceOpenInBrowser.ts
+++ src/composables/useForceOpenInBrowser.ts
@@ -1,10 +1,9 @@
 export const useForceOpenInBrowser = (): void => {
   var ua = navigator.userAgent;
 
-  console.warn(ua.indexOf('FBAN'))
-  console.warn(ua.indexOf('FBAV'))
-  console.warn(window.location.href)
   if (ua.indexOf("FBAN") != -1 || ua.indexOf("FBAV") != -1) {
-    window.location.href = `${window.location.href}?redirect=fb`;
+    if (!window.location.href.includes('?redirect=fb')) {
+      window.location.href = `${window.location.href}?redirect=fb`;
+    }
   }
 }
