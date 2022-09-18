ae410cd27b2c00d4da16d0ee300ccbc1f4f3c85f
Stefano Salidu <st.salidu@gmail.com>
2022-09-18 21:13:07 +0200

    chore: updated force open in browser

--- src/composables/useForceOpenInBrowser.ts
+++ src/composables/useForceOpenInBrowser.ts
@@ -1,9 +1,25 @@
 export const useForceOpenInBrowser = (): void => {
-  var ua = navigator.userAgent;
+  const openInBrowser = (target: string, browserScheme: string) => {
+    var ifc = document.createElement("div")
+    ifc.innerHTML = `<iframe src='${browserScheme}${target}' style='width:0;height:0;border:0; border:none;visibility: hidden;'></iframe>`
+    document.body.appendChild(ifc)
+  }
 
-  if (ua.indexOf("FBAN") != -1 || ua.indexOf("FBAV") != -1) {
-    if (!window.location.href.includes('?redirect=fb')) {
-      window.location.href = `${window.location.href}?redirect=fb`;
+  const isInApp = (appSpecificUserAgents: string[]): boolean => {
+    var userAgent = navigator.userAgent || navigator.vendor
+    return appSpecificUserAgents
+      .some(ua => userAgent.includes(ua))
+  }
+
+  const tryOpenBrowser = (): void => {
+    if (document.body) {
+      if (isInApp(['FBAN', 'FBAV'])) {
+        openInBrowser(window.location.href, 'googlechrome://navigate?url=')
+      }
+    } else {
+      window.requestAnimationFrame(tryOpenBrowser);
     }
   }
+
+  onMounted(() => tryOpenBrowser())
 }
