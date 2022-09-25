08f233afa360ab7fcb1bb1fc60e9be7d40299906
Stefano Salidu <st.salidu@gmail.com>
2022-09-25 11:33:02 +0200

    chore: udpated package

--- src/components.d.ts
+++ src/components.d.ts
@@ -3,8 +3,6 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
-export {}
-
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Article: typeof import('./components/common/typography/Article.vue')['default']
@@ -37,3 +35,5 @@ declare module '@vue/runtime-core' {
     Tooltip: typeof import('./components/common/tooltip/Tooltip.vue')['default']
   }
 }
+
+export {}
